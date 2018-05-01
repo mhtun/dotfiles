27e016e9451081f30ddc5afb04ce39cf8141893c
Min Tun <mintun@iinet.net.au>
2018-05-01 04:20:58 -0700

    added some more messages

--- install_nvim.sh
+++ install_nvim.sh
@@ -7,12 +7,16 @@ sudo apt install neovim python-neovim python3-neovim
 echo "creating directories"
 echo "********************"
 mkdir -p ~/.config/nvim/plugged
+echo "created ~/.config/nvim/plugged"
 mkdir -p ~/.config/nvim/autoload
+echo "created ~/.config/nvim/autoload"
 
 echo "downloading vim-plug"
 echo "********************"
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "download complete"
 
 echo "installing nvim config"
 echo "**********************"
 cp init.vim ~/.config/nvim/
+echo "install complete"
